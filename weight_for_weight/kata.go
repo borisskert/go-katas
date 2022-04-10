4af89f88010bb9d961b6de68f7bbcd730ed3e797
Boris Skert <boris.skert@gmail.com>
2022-04-10 09:21:10 +0200

    Solve 'Weight for weight' kata

---
+++ weight_for_weight/kata.go
@@ -0,0 +1,78 @@
+package kata
+
+import (
+	"sort"
+	"strings"
+)
+
+// OrderWeight https://www.codewars.com/kata/55c6126177c9441a570000cc/train/go
+func OrderWeight(strng string) string {
+	return Convert(strng).
+		Sorted().
+		ToString()
+}
+
+type Weight struct {
+	crossSum int
+	raw      string
+}
+
+type Weights struct {
+	items []Weight
+}
+
+func Convert(strng string) Weights {
+	words := strings.Fields(strng)
+
+	var weights []Weight
+
+	for _, raw := range words {
+		weight := NewWeight(raw)
+		weights = append(weights, weight)
+	}
+
+	return Weights{weights}
+}
+
+// Sorted sorts the weights by creating a new instance
+func (ctx Weights) Sorted() Weights {
+	sorted := make([]Weight, len(ctx.items))
+	copy(sorted, ctx.items)
+
+	sort.SliceStable(sorted, func(i, j int) bool {
+		a := sorted[i]
+		b := sorted[j]
+
+		if a.crossSum == b.crossSum {
+			return a.raw < b.raw
+		}
+
+		return a.crossSum < b.crossSum
+	})
+
+	return Weights{sorted}
+}
+
+func (ctx Weights) ToString() string {
+	var newArray []string
+
+	for _, it := range ctx.items {
+		newArray = append(newArray, it.raw)
+	}
+
+	return strings.Join(newArray, " ")
+}
+
+func NewWeight(raw string) Weight {
+	return Weight{crossSumOf(raw), raw}
+}
+
+func crossSumOf(raw string) int {
+	var crossSum = 0
+
+	for _, element := range []rune(raw) {
+		crossSum += int(element - '0')
+	}
+
+	return crossSum
+}
---
+++ weight_for_weight/kata_test.go
@@ -0,0 +1,30 @@
+package kata
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func dotest(s string, exp string) {
+	var ans = OrderWeight(s)
+	Expect(ans).To(Equal(exp))
+}
+
+type MyString struct {
+	value string
+}
+
+var _ = Describe("Tests OrderWeight", func() {
+
+	It("should handle basic cases", func() {
+		dotest("103 123 4444 99 2000", "2000 103 123 4444 99")
+		dotest("2000 10003 1234000 44444444 9999 11 11 22 123", "11 11 2000 10003 22 123 1234000 44444444 9999")
+		dotest("", "")
+	})
+})
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Weight for weight")
+}
