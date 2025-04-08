ea19a82d8c4d8cdd7836106eea4de272221f4e2b
Boris Skert <boris.skert@byteagenten.de>
2025-04-08 14:08:17 +0200

    Simplify tests

--- can_you_get_the_loop/kata_test.go
+++ can_you_get_the_loop/kata_test.go
@@ -1,6 +1,9 @@
 package kata_test
 
 import (
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "go-katas/can_you_get_the_loop"
@@ -58,3 +61,72 @@ var _ = Describe("FindLoop", func() {
 		Expect(FindLoop(root)).To(Equal(3000))
 	})
 })
+
+var _ = Describe("FindLoop (Extended tests)", func() {
+	It("finds loop in list of random nodes (between 1 and 10 nodes)", func() {
+		loopSize := uint(randomUint64Range(1, 10))
+		root := CreateRandomChain(uint(randomUint64Range(1, 10)), loopSize)
+		Expect(FindLoop(root)).To(Equal(int(loopSize)))
+	})
+
+	It("finds loop in list of random nodes (between 10 and 100 nodes)", func() {
+		loopSize := uint(randomUint64Range(10, 100))
+		root := CreateRandomChain(uint(randomUint64Range(10, 100)), loopSize)
+		Expect(FindLoop(root)).To(Equal(int(loopSize)))
+	})
+
+	It("finds loop in list of random nodes (between 100 and 1000 nodes)", func() {
+		loopSize := uint(randomUint64Range(100, 1000))
+		root := CreateRandomChain(uint(randomUint64Range(100, 1000)), loopSize)
+		Expect(FindLoop(root)).To(Equal(int(loopSize)))
+	})
+
+	It("finds loop in list of random nodes (between 1000 and 10000 nodes)", func() {
+		loopSize := uint(randomUint64Range(1000, 10000))
+		root := CreateRandomChain(uint(randomUint64Range(1000, 10000)), loopSize)
+		Expect(FindLoop(root)).To(Equal(int(loopSize)))
+	})
+
+	It("finds loop in list of random nodes (between 10000 and 100000 nodes)", func() {
+		loopSize := uint(randomUint64Range(10000, 100000))
+		root := CreateRandomChain(uint(randomUint64Range(10000, 100000)), loopSize)
+		Expect(FindLoop(root)).To(Equal(int(loopSize)))
+	})
+
+	It("finds loop in list of random nodes (between 100000 and 1000000 nodes)", func() {
+		loopSize := uint(randomUint64Range(100000, 1000000))
+		root := CreateRandomChain(uint(randomUint64Range(100000, 1000000)), loopSize)
+		Expect(FindLoop(root)).To(Equal(int(loopSize)))
+	})
+})
+
+func randomUint64Range(minValue, maxValue uint64) uint64 {
+	if minValue >= maxValue {
+		panic("min must be less than max")
+	}
+
+	rangeSize := maxValue - minValue
+
+	randomOffset := randomUint64N(rangeSize)
+
+	return minValue + randomOffset
+}
+
+func randomUint64N(n uint64) uint64 {
+	if n == 0 {
+		panic("n must be greater than 0")
+	}
+
+	var num uint64
+
+	err := binary.Read(rand.Reader, binary.LittleEndian, &num)
+	if err != nil {
+		panic(fmt.Errorf("failed to generate random number: %w", err))
+	}
+
+	if num < n {
+		return num
+	}
+
+	return num % n
+}
